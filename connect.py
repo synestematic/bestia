017ecc70278c1b2461fdbf4f261d12fb09008701
taito <taito@pro15.local>
2018-10-07 19:03:07 +0200

    add verbose option to http_get()

--- connect.py
+++ connect.py
@@ -4,10 +4,9 @@ from time import sleep
 
 from .output import echo, abort
 
-def http_get(url, retries=3):
+def http_get(url, retries=3, verbose=False):
 	try:
 		headers = {
-			# 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
 			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
 			# 'cookie': '__cfduid=dc007f70cc6e84fe5cf05b13b9d7022361536178218; _ga=GA1.2.1741958857.1536178240; _gid=GA1.2.1955541911.1536178240; ppu_main_b1f57639c83dbef948eefa8b64183e1e=1; ppu_sub_b1f57639c83dbef948eefa8b64183e1e=1; _gat=1'
 		}
@@ -15,19 +14,22 @@ def http_get(url, retries=3):
 		if r.status_code == 200:
 			html = r.text
 			if type(html) != str:
-				echo('http request returned {} insetad of <str>'.format(type(html)), 'red')
+				if verbose:
+					echo('http request returned {} insetad of <str>'.format(type(html)), 'red')
 				return
 			return html
 		else:
 			return
 	except requests.exceptions.ConnectionError:
 		if retries < 1:
-			echo('Unable to connect', 'red')
+			if verbose:
+				echo('Unable to connect', 'red')
 			return
 		else:
-			echo('No connection... {} attempts left'.format(retries), 'red')
+			if verbose:
+				echo('No connection, {} attempts left...'.format(retries), 'red')
 			sleep(5)
-			http_get(url, retries=retries-1)
+			http_get(url, retries=retries-1, verbose=verbose)
 
 if __name__ == '__main__':
 	pass
