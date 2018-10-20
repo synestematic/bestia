4fbdbb6c9b3b6a21f089425b5480f922a77b75ce
montypython <2montypython@gmx.com>
2018-10-20 21:06:40 +0200

    create distribution environment

---
+++ README.md
@@ -0,0 +1,5 @@
+# carolina
+
+* what
+* the
+* fuck
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="carolina",
+    version="0.0.1",
+    author="Federico Rizzo",
+    author_email="yo@gmail.com",
+    description="Tools for interacting with web services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/synestematic/carolina",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
+
