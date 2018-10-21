4611badfa2c110493c6924268ac7a5f73e3368f6
montypython <2montypython@gmx.com>
2018-10-22 00:43:56 +0200

    clean-up after install

--- build_install_package.sh
+++ build_install_package.sh
@@ -3,11 +3,16 @@
 
 PACKAGE=bestia
 PIP_VERSION=pip3
+PURGE_DIRS=(dist build "$PACKAGE".egg-info)
 
 # build
 python3 setup.py sdist bdist_wheel || exit
 
 # install
-
 "$PIP_VERSION" show "$PACKAGE" &>/dev/null && "$PIP_VERSION" uninstall "$PACKAGE" -y
-cd dist && "$PIP_VERSION" install "$PACKAGE"-0.0.1-py3-none-any.whl
+cd dist && "$PIP_VERSION" install "$PACKAGE"-0.0.1-py3-none-any.whl && cd ..
+
+# clean-up
+for dir in "${PURGE_DIRS[@]}"; do
+    [ -d "$dir" ] && rm -rf "$dir" && echo "deleted $dir directory"
+done
