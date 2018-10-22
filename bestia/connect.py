f36a35ebabba3cac3716ed2febc3b25f0b91a7f6
montypython <2montypython@gmx.com>
2018-10-22 09:38:58 +0200

    improve http_get

--- bestia/connect.py
+++ bestia/connect.py
@@ -2,13 +2,14 @@ import sys
 import requests
 from time import sleep
 
-from bestia.output import echo, abort
+from bestia.output import echo
 
-def http_get(url, retries=3, verbose=False):
+def http_get(url, retries=3, verbose=False, browser='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'):
 	try:
 		headers = {
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-			# 'cookie': '__cfduid=dc007f70cc6e84fe5cf05b13b9d7022361536178218; _ga=GA1.2.1741958857.1536178240; _gid=GA1.2.1955541911.1536178240; ppu_main_b1f57639c83dbef948eefa8b64183e1e=1; ppu_sub_b1f57639c83dbef948eefa8b64183e1e=1; _gat=1'
+			'User-Agent': browser,
+			# 'Referer': referer,
+			# 'Cookie': '__cfduid=dc007f70cc6e84fe5cf05b13786ac361536178218; _ga=GA1.2.1741958857.1512378240; _gid=GA1.2.1955541911.1536178240; ppu_main_b1f57639c83dbef948eefa8b64183e1e=1; ppu_sub_b1f57639c83dbef9488abc6b64183e1e=1; _gat=1'
 		}
 		r = requests.get(url, headers=headers)
 		if r.status_code == 200:
@@ -16,21 +17,20 @@ def http_get(url, retries=3, verbose=False):
 			if type(html) != str:
 				if verbose:
 					echo('http request returned {} insetad of <str>'.format(type(html)), 'red')
-				return
+				return False
 			return html
 		else:
-			return
+			return False
 	except requests.exceptions.ConnectionError:
 		if retries < 1:
 			if verbose:
 				echo('Unable to connect', 'red')
-			return
+			return False
 		else:
 			if verbose:
 				echo('No connection, {} attempts left...'.format(retries), 'red')
 			sleep(5)
-			http_get(url, retries=retries-1, verbose=verbose)
+			http_get(url, retries=retries-1, verbose=verbose, browser=browser)
 
 if __name__ == '__main__':
 	pass
-	# implement referrers and cookies
