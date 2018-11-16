9c3cffdaa8555d3e92343d89950689bfc1d9d4ce
montypython <2montypython@gmx.com>
2018-11-17 00:52:25 +0100

    upload to PyPI

--- package.sh
+++ package.sh
@@ -13,12 +13,11 @@ python3 setup.py sdist bdist_wheel || exit
 "$PIP_VERSION" show "$PACKAGE" &>/dev/null && "$PIP_VERSION" uninstall "$PACKAGE" -y
 cd dist && "$PIP_VERSION" install "$PACKAGE"-"$PACKAGE_VERSION"-py3-none-any.whl && cd ..
 
-# clean-up
+# upload to PyPI
+twine upload dist/*
+
+# clean up
 for dir in "${PURGE_DIRS[@]}"; do
     [ -d "$dir" ] && rm -rf "$dir" && echo "deleted $dir directory"
 done
-
-# upload
-# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-
 
