6e6b8c07bafa57133cbde782bc29fd9fb12e750e
montypython <2montypython@gmx.com>
2018-11-27 16:42:51 +0100

    provide feedback on package.sh

--- package.sh
+++ package.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 # https://packaging.python.org/tutorials/packaging-projects/
 
+echo_green() { echo -e "\033[01;32m$1\033[00m"; }
+echo_magenta() { echo -e "\033[01;35m$1\033[00m"; }
+
 PACKAGE=bestia
 PACKAGE_VERSION=`cat setup.py | awk -F"\"|\'" '/version/{print $2}'`
 PIP_VERSION=pip3
 PURGE_DIRS=(dist build "$PACKAGE".egg-info)
 
 # build
-python3 setup.py sdist bdist_wheel || exit
+echo_magenta "Building $PACKAGE $PACKAGE_VERSION"
+python3 setup.py sdist bdist_wheel && echo_green "Build OK" || exit
 
 # install
-"$PIP_VERSION" show "$PACKAGE" &>/dev/null && "$PIP_VERSION" uninstall "$PACKAGE" -y
+"$PIP_VERSION" show "$PACKAGE" &>/dev/null && (echo_magenta "Uninstalling old $PACKAGE version"; "$PIP_VERSION" uninstall "$PACKAGE" -y )
+echo_magenta "Installing $PACKAGE $PACKAGE_VERSION"
 cd dist && "$PIP_VERSION" install "$PACKAGE"-"$PACKAGE_VERSION"-py3-none-any.whl && cd ..
 
 # upload to PyPI
-[ "$1" == '--upload' ] && twine upload dist/*
+[ "$1" == '--upload' ] && (echo_magenta "Uploading to PyPI "; twine upload dist/*)
 
 # clean up
+echo_magenta "Cleaning up"
 for dir in "${PURGE_DIRS[@]}"; do
-    [ -d "$dir" ] && rm -rf "$dir" && echo "deleted $dir directory"
+    [ -d "$dir" ] && rm -rf "$dir" && echo "Deleted $dir directory"
 done
