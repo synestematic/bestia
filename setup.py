5fed6e2229bf9dba02555fff0587eb4bbcb07815
montypython <2montypython@gmx.com>
2018-12-20 23:17:34 +0100

    version 0.8.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bestia",
-    version="0.8.6",
+    version="0.8.7",
     author="Federico Rizzo",
     author_email="foo@bar.com",
     description="A collection of tools for building dynamic Command-Line applications",
