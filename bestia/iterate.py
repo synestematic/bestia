fe5006c1ee43852b1381b5482ff64306520035b6
montypython <2montypython@gmx.com>
2019-01-28 13:03:53 +0100

    default to 1 number of random items from list

--- bestia/iterate.py
+++ bestia/iterate.py
@@ -1,15 +1,12 @@
 from random import randrange, choice
 
-
 def items_are_equal(iterable):
    return iterable[1:] == iterable[:-1]
-
 
 def indexes_from_string(string):
     return [ i for i in range(len(str(string))) ]
 
-
-def random_unique_items_from_list(lst, amount):
+def random_unique_items_from_list(lst, amount=1):
     items = []
     while amount != 0:
         random_item = choice(lst)
@@ -18,7 +15,6 @@ def random_unique_items_from_list(lst, amount):
             amount -= 1
     return items
 
-
 def pop_random_item(lst):
     try:
         random_index = randrange(len(lst))
@@ -26,13 +22,14 @@ def pop_random_item(lst):
     except ValueError:
         return
 
-
 def string_to_list(string):
     return [ c for c in str(string) ]
-
 
 def list_to_string(lst):
     string = ''
     for c in lst:
         string = string + c
     return string
+
+if __name__ == '__main__':
+    pass
