a5cf9feb21ce151192afe9aae39038d5b034ab12
Julio Sosa <bla@abz.com>
2020-11-13 12:00:05 +0100

    version 3.1

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name="bestia",
-    version="3.0.3",
+    version="3.1",
     author="Federico Rizzo",
-    author_email="synestem@tic.com",
+    author_email="synestem@ticATgmail.com",
     description="A collection of tools for building dynamic Command-Line applications",
     long_description=long_description,
     long_description_content_type="text/markdown",
