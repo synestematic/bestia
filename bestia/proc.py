df9f86276c114335e01d519b8db8c4fe524e38af
Julio Sosa <bla@abz.com>
2021-07-14 11:37:34 +0200

    RemoteHost and SshCommand classes

--- bestia/proc.py
+++ bestia/proc.py
@@ -1,10 +1,11 @@
-import subprocess
 import os
-import struct
-import time
 import select
+import struct
+import subprocess
 
-from .output import tty_cols
+from datetime import datetime
+
+from .output import tty_cols, FString, Row
 from .error import *
 
 SUCCESS =  0
@@ -13,6 +14,7 @@ FAILURE = -1
 DEFAULT_STDIN  = 0
 DEFAULT_STDOUT = 1
 DEFAULT_STDERR = 2
+
 
 def change_directory(original_func):
     def decorated_func(*args, **kwargs):
@@ -25,6 +27,7 @@ def change_directory(original_func):
             return rc
         return original_func(*args, **kwargs)
     return decorated_func
+
 
 def which(binary):
     try:
@@ -73,7 +76,6 @@ class Process(object):
         for _ in range(trail_lines):
             print()
 
-
     @change_directory
     def run(self, verbose=2, in_dir=''):
         ''' 
@@ -111,7 +113,6 @@ class Process(object):
 
         return self.rc
 
-
     def _no_shell_run(self, verbose):
         executable, arguments = self.parse_command()
 
@@ -140,7 +141,20 @@ class Process(object):
                     elif readable_fd == stdout_fd:
                         self.stdout += byte[readable_fd]
                         if verbose > 1:
-                            print(byte[readable_fd].decode(), end='', flush=True)
+                            # buffer this bytes until they are utf-8 printable...
+                            try:
+                                print(
+                                    byte[readable_fd].decode(),
+                                    end='',
+                                    flush=True,
+                                )
+                            except:
+                                pass
+                                # print(
+                                #     '?',
+                                #     end='',
+                                #     flush=True,
+                                # )
 
         self.rc = wait_for(child_pid)
         for fd in (stdin_fd, stdout_fd, stderr_fd):
@@ -155,6 +169,7 @@ def wait_for(pid):
     sc = struct.pack("<I", rc)
     retval = struct.unpack("bb", sc[0:2])[1]
     return retval
+
 
 def fork_subproc(path, args):
     ''' runs command on forked (parallel) subproc
@@ -201,3 +216,234 @@ def fork_subproc(path, args):
         os.close(child_STDOUT_write)
         os.close(child_STDERR_write)
         return child_pid, child_STDIN_write, child_STDOUT_read, child_STDERR_read
+
+
+class SSHCommand(Process):
+
+    WHITELIST = (
+        # sudo does not need to be in here  LOL :)
+        'cat',
+        'docker',
+        'find',
+        'ls',
+        'ping',
+        'pwd',
+        'ssh',
+        'wget',
+        'whoami',
+    )
+
+    def __init__(self, command, user, host, timeout=2):
+        # -F /dev/null               discards custom search ssh configs in ~/.ssh/config
+        # -o ConnectTimeout=2        avoids waiting for unresponsive hosts
+        # -o ServerAliveInterval=60  client sends null packet to server to keep connection alive
+        #                            avoids zombie ssh sessions...
+        super().__init__(
+            'ssh -F /dev/null -o ServerAliveInterval=60 -o ConnectTimeout={} {}@{} {}'.format(
+                timeout, user, host, command
+            )   
+        )
+        self.user = user
+        self.host = host
+
+    def banner(self, msg='', sep='\\', lead_lines=0, trail_lines=0):
+
+        for arg in self.command.split():
+
+            if arg.startswith(f'{self.user}@'):
+                ssh_command = f'{self.command.split(arg)[0]}{arg}'
+                # ssh -F /dev/null -o ServerAliveInterval=60 -o ConnectTimeout=2 user@my.host.com sudo whoami
+                host = ssh_command.split(f'{self.user}@')[-1] # my.host.com
+                ssh_command = ssh_command.replace(host, '')   # ssh -F /dev/null -o ServerAliveInterval=60 -o ConnectTimeout=2 user@
+                rmt_command = self.command.split(arg)[1]      # sudo whoami
+
+                Row(
+                    FString(
+                        ssh_command,
+                        size=len(ssh_command),
+                        fx=['faint'],
+                    ),
+                    FString(
+                        host,
+                        size=len(host),
+                        fg='cyan',
+                        # fx=['bold'],
+                    ),
+                    FString(
+                        rmt_command,
+                        size=len(rmt_command),
+                        fg='blue',
+                    ),
+                    FString(
+                        f'{msg} ',
+                        pad=sep,
+                        fx=['bold'],
+                    )
+                ).echo(mode='retro')
+
+                if 'exit=' in msg: print()
+
+                return
+
+        super().banner(msg=msg, sep=sep, lead_lines=lead_lines, trail_lines=trail_lines)
+
+
+
+def _log_host_command(host, cmd, o , e):
+    """ logs commands stdout and stderr for non-ephemeral usage
+    """
+    datehost_dir = _create_datehost_dir(host)
+    if not datehost_dir:
+        return FAILURE
+
+    try:
+        log_cmd_dir = '{datehost_dir}/[{time}] {cmd}'.format(
+            datehost_dir=datehost_dir,
+            time=datetime.now().strftime('%H:%M:%S'),
+            cmd=cmd.replace('/', '∕'),
+        )
+        os.makedirs(log_cmd_dir, exist_ok=False)
+
+        # CREATE FILES
+        if o:
+            with open(f'{log_cmd_dir}/1', 'wb') as stdout:
+                stdout.write(o)
+
+        if e:
+            with open(f'{log_cmd_dir}/2', 'wb') as stderr:
+                stderr.write(e)
+
+        return SUCCESS
+
+    except:
+        return FAILURE
+
+
+class RemoteHost(object):
+
+    def __init__(self, user, host):
+        self.user = user
+        self.host = host
+
+    def ssh_run(self, cmd, sudo=False, strict=False, timeout=2, verbose=1, log=False, risk=False):
+        """ sudo   = executes ssh command as root
+            strict = raises exception if rc is not 0
+            log    = store results locally for logging
+            risk   = ignores COMMAND_WHITELIST, allows execution of any command
+        """
+        # ONLY RUN IF NODE IS NOT DISABLED IN CC...
+        if not risk and cmd.split()[0] not in SSHCommand.WHITELIST:
+            raise RuntimeError(f'ssh command not allowed -> [{cmd}]')
+
+        cmd = cmd if not sudo else f'sudo {cmd}'
+
+        ssh_proc = SSHCommand(
+            command=cmd,
+            user=self.user,
+            host=self.host,
+        )
+        ssh_proc.run(verbose=verbose)
+
+        if log:
+            _log_host_command(self.host, cmd, ssh_proc.stdout, ssh_proc.stderr)
+
+        if ssh_proc.rc != 0 and strict:
+            raise RuntimeError(f'Failed to run -> {cmd}')
+
+        return ssh_proc
+
+    def docker_exec(self, container, cmd, verbose=1, log=False, risk=False):
+        """ ssh user@my.host.com sudo docker exec my_container cat /var/log/gunicorn/error.log
+        """
+        if not risk and cmd.split()[0] not in SSHCommand.WHITELIST:
+            raise RuntimeError(f'ssh command not allowed -> [{cmd}]')
+        return self.ssh_run(
+            sudo=True,
+            cmd=f'docker exec {container.name} {cmd}',
+            strict=False,
+            verbose=verbose,
+            log=log
+        )
+
+    def get_remote_file(self,
+        remote_filename, remote_directory='',
+        local_filename='', local_directory='',
+        container=None
+    ):
+        """ cats remote_file contents to stdout and writes to local_file
+            DOES NOT work with directories     !
+            DOES NOT keep original permissions !
+        """
+        remote_filepath, local_filepath = _resolve_filepath_locations(
+            remote_filename=remote_filename,
+            remote_directory=remote_directory,
+            local_filename=local_filename,
+            local_directory=local_directory if local_directory else self.create_store_dir(),
+        )
+
+        if container:
+            cmd = self.docker_exec(
+                container=container,
+                cmd=f'cat {remote_filepath}',
+                verbose=1,
+            )
+
+        else:
+            cmd = self.ssh_run(
+                sudo=True,
+                cmd=f'cat {remote_filepath}',
+                verbose=1,  # 1 allows to monitor file being read, 2 risks printing non printable bytes to screen
+            )
+
+        # this WILL overwrite your local files!
+        try:
+            with open(local_filepath, 'wb') as f:
+                f.write(cmd.stdout)
+            return SUCCESS
+        except:
+            return FAILURE
+
+    def create_store_dir(self):
+        created_dir = _create_datehost_dir(self.host)
+        if not created_dir:
+            raise Exception('Failed to create store dir')
+        return created_dir
+
+
+def _create_datehost_dir(host):
+    """ /home/user/S1/logs/2021-02-17-Wed/my.host.com
+    """
+    try:
+        datehost_dir = '/home/{user}/S1/logs/{date}/{host}'.format(
+            user=os.environ['USER'],
+            date=datetime.now().strftime('%Y-%m-%d-%a'),
+            host=host,
+        )
+        os.makedirs(datehost_dir, exist_ok=True)
+        return datehost_dir
+    except:
+        return ''
+
+
+def _resolve_filepath_locations(remote_filename, remote_directory, local_filename, local_directory):
+    """ if remote_directory is not specified, remote_file must be in ssh_user home dir
+        if local_directory  is not specified, local_file  will be put in local CWD
+        if local_filename   is not specified, local_file  will have same name as remote_filename
+    """
+    if remote_directory:
+        remote_directory = remote_directory.rstrip('/')
+        remote_filepath = f'{remote_directory}/{remote_filename}'
+    else:
+        remote_filepath = remote_filename
+
+    if not local_filename:
+        local_filename = remote_filename
+
+    if local_directory:
+        local_directory = local_directory.rstrip('/')
+        local_filepath = f'{local_directory}/{local_filename}'
+
+    else:
+        local_filepath = local_filename
+
+    return remote_filepath, local_filepath
